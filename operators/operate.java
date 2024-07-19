60182937ddbf573cccf2cc6933884e58432634fc
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-20 02:17:06 +0530

    performing operations

---
+++ operators/operate.java
@@ -0,0 +1,11 @@
+class operate{
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 5;
+
+        int result = num1 * num1;
+
+        System.out.println(result);
+
+    }
+}

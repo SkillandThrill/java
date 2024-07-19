190ac3a2d67e2e3f3af571171d2bb2f9ecd8d575
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-20 02:09:57 +0530

    initial commit

---
+++ casting/cast.java
@@ -0,0 +1,35 @@
+public class cast{
+    public static void main(String[] args) {
+        int myInt =9;
+        double myDouble = myInt; // Automatic casting int to double
+
+        System.out.println(myInt); //Outputs 9
+        System.out.println(myDouble); //Outputs 9.0
+
+        double myDoublee = 9.787d;
+        int myIntt = (int)myDoublee;
+
+        System.out.println(myDoublee);
+        System.out.println(myIntt);
+
+
+        // byte b = 127;
+        // int a= 256;
+
+        // byte k = a;
+        // System.out.println(k);
+
+
+        // float f =5.6f;
+        // int t = (int) f;
+
+        // System.out.println(t);
+
+
+        byte a =10;
+        byte b =30;
+
+        int result = a*b;
+        System.out.println(result);
+    }
+}
---
+++ course/hello.java
@@ -0,0 +1,37 @@
+class hello{
+    public static void main(String a[]){
+
+        // double num = 7.63;
+        // System.out.println(num);
+
+        // string name = "nigga";
+
+        // System.out.println(name);
+
+        // boolean true;
+        // bool b = true;
+
+
+        int num1 =9;
+        byte count = 22;
+        short sh = 558;
+        long l = 3462838l;
+        float f = 5.8f;
+        double d = 223.312;
+        char c = 'g';
+        boolean b= true;
+
+        System.out.println(num1);
+        System.out.println(count);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+
+
+        int  num =0b101;
+        System.out.println(num);
+    }
+}

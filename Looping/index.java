187c72ebdcac03f658f6ffb96a440be52049f1a9
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-25 19:11:22 +0530

    started learning looping with while looked how the debugging actuallyb done! and continuing with next type of looping

---
+++ Looping/index.java
@@ -0,0 +1,22 @@
+// package Looping;
+
+public class index {
+    public static void main(String[] args) {
+
+        // for(int i=0;i<4;i++){
+        //     System.out.println( "hi");
+
+        // }
+
+
+
+        // while l  oop
+
+        int i=1;
+        while(i<=4){
+        System.out.println("Hello " + i);
+        i++;
+        }
+        System.out.println("Bye " + i);
+    }
+}
--- conditionals/switchState.java
+++ conditionals/switchState.java
@@ -3,14 +3,32 @@ package conditionals;
 public class switchState {
     public static void main(String[] args) {
         
-        int n =1;
+        int n =20;
 
-        System.out.println("Monday");
-        System.out.println("Tuesday");
-        System.out.println("Wednesday");
-        System.out.println("Thursday");
-        System.out.println("Friday");
-        System.out.println("Saturday");
-        System.out.println("Sunday");
+        switch(n){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+               System.out.println("Tuesday");
+               break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("enter a valid number");
+        }
     }
 }

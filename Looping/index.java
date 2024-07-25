b20e4b8a4d2a2e716bdcdc6496ea20b3b056547c
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-26 00:40:55 +0530

    started the looping with java and done some examples on various looping types like while do-while and for which is the most common one among the others.

--- Looping/index.java
+++ Looping/index.java
@@ -12,11 +12,34 @@ public class index {
 
         // while l  oop
 
-        int i=1;
-        while(i<=4){
-        System.out.println("Hello " + i);
-        i++;
+        // int i=1;
+        // while(i<=3){
+        // System.out.println("Hello " + i);
+
+        // int j = 1;
+            
+        // while(j<=3){
+        //     System.out.println("Hi " + j);
+        //     j++;
+        // }
+        // i++;
+        // }
+        // System.out.println("Bye " + i);
+
+
+        //do while loop
+        // int i = 5;
+        // do
+        // {
+        //     System.out.println("HI " + i);
+        //     i++;
+        // }
+        // while(i<=4);
+
+        //for loop
+        for(int i = 0;i<4;i++){
+            System.out.println("Hi " + i);
         }
-        System.out.println("Bye " + i);
+
     }
 }

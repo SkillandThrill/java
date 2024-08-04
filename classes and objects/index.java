a257f993451c3893da58d13abd8f9fb93301bf81
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-08-04 23:40:07 +0530

    started the concept of classes and objects and solved some mathematical problems to understand the basic concepts:

--- classes and objects/index.java
+++ classes and objects/index.java
@@ -1,9 +1,20 @@
 // package classes and objects;
 
+
+
 public class index {
     public static void main(String[] args) {
 
-        System.out.println();
+        int num1=8;
+        int num2=4;
+        int result = num1+num2;
+        System.out.println(result);
     }
     
 }
+
+// object oriented programming 
+//object has the properties and the behaviors
+
+//to create a object have to create the class first
+

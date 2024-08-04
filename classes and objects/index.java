014ea266369c186a623ce4b7a29d8f0d06a4ea0e
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-08-05 02:33:03 +0530

    adding changes to the repo

--- classes and objects/index.java
+++ classes and objects/index.java
@@ -1,13 +1,29 @@
 // package classes and objects;
 
+class Calculator
+{
 
+    // int a;
+
+    public int add(int n1,int n2)
+    {
+
+        int r = n1 +n2;
+        return r;
+    }
+}
 
 public class index {
     public static void main(String[] args) {
 
         int num1=8;
         int num2=4;
-        int result = num1+num2;
+        // int result = num1+num2;
+
+        Calculator calc = new Calculator();
+
+        int result = calc.add(6,4);
+
         System.out.println(result);
     }
     

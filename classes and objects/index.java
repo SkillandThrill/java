f353a2226ef241e794a906e839cb35277dc0f3c3
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-08-06 23:35:56 +0530

    commiting changes

--- classes and objects/index.java
+++ classes and objects/index.java
@@ -1,36 +1,36 @@
 // package classes and objects;
 
-class Calculator
-{
+class Calculator {
 
     // int a;
+    public int add(int n1, int n2) {
 
-    public int add(int n1,int n2)
-    {
-
-        int r = n1 +n2;
+        int r = n1 + n2;
         return r;
     }
 }
 
 public class index {
+
     public static void main(String[] args) {
 
-        int num1=8;
-        int num2=4;
+        int num1 = 8;
+        int num2 = 4;
         // int result = num1+num2;
 
         Calculator calc = new Calculator();
 
-        int result = calc.add(6,4);
+        int result = calc.add(6, 4);
 
         System.out.println(result);
     }
-    
+
 }
 
 // object oriented programming 
 //object has the properties and the behaviors
-
 //to create a object have to create the class first
+
+//
+// ?
 

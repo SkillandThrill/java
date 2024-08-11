9a632bc469b13b60749a3a8f4c95e755235c5302
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-08-11 15:48:20 +0530

    created the class and with the basic example of adding leared to pass and accept the values from one class to another and execute the operation.

--- objects/index.java
+++ objects/index.java
@@ -1,9 +1,30 @@
 // package objects;
 
+class Calculator
+{
+    public int add(int a ,int b)
+    {
+        int r = a+b;
+        return  r;
+    }
+}
+
+
 public class index {
         public static void main(String[] args) {
 
-            System.out.println("hello");
+            int a = 3;
+            int b =8;
+
+            Calculator calc = new Calculator();
+            int result =  calc.add(a,b);
+            
+            System.out.println(result);
+        
+
+
+
+            // int result = a + b;
         }
     
 }
